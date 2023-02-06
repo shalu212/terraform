22cfca97375c0e5bd5c505c0be266d6ef2fdedca
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-06 16:55:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 #Aws As Our Provider
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA3UJ2NXQFASH3XOAB
-"
+  region     = "ap-south-1"
+  access_key = "AKIA3UJ2NXQFASH3XOAB"
   secret_key = "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"
+}
